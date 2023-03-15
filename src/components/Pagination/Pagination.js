5fafd214bc2f723cabebd879b9f33756a581f6a5
Kosynskyi <kosynskyi.vova@gmail.com>
2023-03-15 12:47:34 +0200

    fix bugs in pagination

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,47 +1,52 @@
 import React, { useState, useEffect } from 'react';
 import { ITEM_PER_PAGE } from 'helpers/constants';
 import { useGetAllCoursesQuery } from 'redux/CourseSlice';
+import Skeleton from 'components/Skeleton';
 import CourseList from 'components/CourseList';
 import { PaginateList, ButtonNext, ButtonPrev } from './Pagination.styled';
 
 const Pagination = () => {
   const [currentItems, setCurrentItems] = useState(null);
   const [currentPage, setCurrentPage] = useState(
-    localStorage.getItem('currentPage') ?? 1
+    Number(localStorage.getItem('currentPage')) ?? 1
   );
   const [pageCount, setPageCount] = useState(0);
   const [itemOffset, setItemOffset] = useState(0);
-  const { data } = useGetAllCoursesQuery();
+  const { data, isLoading } = useGetAllCoursesQuery();
   const totalCourses = data?.courses.length;
 
   useEffect(() => {
-    setItemOffset(Number(localStorage.getItem('itemOffset')) ?? 0); //? кинуть вiдразу в стейт?
+    setItemOffset(JSON.parse(localStorage.getItem('itemOffset')) ?? 0);
     const endOffset = itemOffset + ITEM_PER_PAGE;
     setCurrentItems(data?.courses.slice(itemOffset, endOffset));
     setPageCount(Math.ceil(totalCourses / ITEM_PER_PAGE));
   }, [data?.courses, itemOffset, totalCourses]);
 
   const handlePageClick = event => {
-    const newOffset = (event.selected * ITEM_PER_PAGE) % data?.courses.length;
+    const newOffset = (event.selected * ITEM_PER_PAGE) % totalCourses;
     localStorage.setItem('itemOffset', newOffset);
     setItemOffset(newOffset);
     setCurrentPage(localStorage.setItem('currentPage', event.selected + 1));
   };
 
-  if (!currentItems) return;
-
   return (
     <>
-      <CourseList currentItems={currentItems} />
-      <PaginateList
-        breakLabel="..."
-        nextLabel={<ButtonNext />}
-        previousLabel={<ButtonPrev />}
-        onPageChange={handlePageClick}
-        pageCount={pageCount}
-        renderOnZeroPageCount={null}
-        forcePage={currentPage ? currentPage - 1 : 0}
-      />
+      {isLoading ? (
+        <Skeleton />
+      ) : (
+        <>
+          <CourseList currentItems={currentItems} />
+          <PaginateList
+            breakLabel="..."
+            nextLabel={<ButtonNext />}
+            previousLabel={<ButtonPrev />}
+            onPageChange={handlePageClick}
+            pageCount={pageCount}
+            renderOnZeroPageCount={null}
+            forcePage={currentPage && currentPage - 1}
+          />
+        </>
+      )}
     </>
   );
 };
