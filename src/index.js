9eebf7c98181443b3219a2d79a3da466dd5733b4
Kosynskyi <kosynskyi.vova@gmail.com>
2023-03-18 15:34:19 +0200

    setup gh-pages

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { theme } from 'theme';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/test-task-gen">
       <Provider store={store}>
         <ThemeProvider theme={theme}>
           <App />
